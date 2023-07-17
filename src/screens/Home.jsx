794b9af7b89e8dbcb75d4dfd377e535e10efef06
Yash Pandey <yashdevesh10@gmail.com>
2023-07-18 00:37:23 +0530

    3rd part completed... almost made a rough frontend

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import Card from '../Components/Card'
+import Carousel from '../Components/Carousel'
 
 export default function Home() {
-  return (
-    <div>
-        <div><Navbar/></div>
-        <div>Body</div>
-        <div><Footer/></div>
-    </div>
-  )
+    return (
+        <div>
+            <div><Navbar /></div>
+            <div><Carousel/></div>
+            <div className='m-3'>
+                <Card />
+                <Card />
+            </div>
+            
+            <div><Footer /></div>
+        </div>
+    )
 }
